b480cdc16fee8b16c20bd805b975dd0a38857601
OnemanWolf <timothy.oleson@gmail.com>
2024-01-26 22:58:01 -0500

    first commit

---
+++ src/app.py
@@ -0,0 +1,42 @@
+from employee import Employee
+from repository import Repository
+# # Import the required libraries
+# from azure.cosmos import CosmosClient, PartitionKey
+# import os
+# # Define the connection string and database details
+# endpoint = os.getenv('END_POINT', 'END_POINT')
+# key = os.getenv('KEY', 'KEY')
+# database_name = os.getenv('DATABASE_NAME', 'END_POINT')
+# container_name = os.getenv('CONTAINER_NAME', 'END_POINT')
+
+# # Create a Cosmos DB client
+# client = CosmosClient(endpoint, key)
+# client.create_database_if_not_exists(database_name)
+# # Get a reference to the database
+# database = client.get_database_client(database_name)
+# # container = database.create_container_if_not_exists(
+# #     id=container_name,
+# #     partition_key=PartitionKey(path="/id")
+# # )
+
+
+# # Get a reference to the container
+# container = database.get_container_client(container_name)
+
+# # Define the JSON document to be inserted
+document = {
+    "id": "emp002",
+    "name": "Jane Doe",
+    "age": 30
+}
+
+# # Insert the document into the container
+# container.create_item(body=document)
+
+# print("Document inserted successfully!")
+repo = Repository()
+
+repo.create(Employee("emp006", "John Doe", 25))
+repo.update("emp006", Employee("emp006", "John Doe", 26))
+repo.delete("emp006")
+repo.read()
---
+++ src/repository.py
@@ -0,0 +1,57 @@
+from azure.cosmos import CosmosClient, PartitionKey
+import os
+endpoint = os.getenv('END_POINT', 'END_POINT')
+key = os.getenv('KEY', 'KEY')
+database_name = os.getenv('DATABASE_NAME', 'END_POINT')
+container_name = os.getenv('CONTAINER_NAME', 'END_POINT')
+client = CosmosClient(endpoint, key)
+client.create_database_if_not_exists(database_name)
+database = client.get_database_client(database_name)
+container = database.get_container_client(container_name)
+import json
+
+class Repository:
+    def __init__(self):
+        self.data = []
+
+        # Create a Cosmos DB client
+
+    def create(self, employee):
+
+        employee_document =  {
+                               "id": employee.id,
+                               "name": employee.name,
+                               "age": employee.age
+                              }
+        PartitionKey = employee.id
+        #value = container.read_item(item=employee.id, partition_key=PartitionKey)
+        employee_exist = container.read_all_items()
+        for item in employee_exist:
+            if item['id'] == employee.id:
+                 print("Document already exists")
+                 return
+        container.create_item(body=employee_document)
+
+        print("Document inserted successfully!")
+    def read(self):
+        data = container.read_all_items()
+        for item in data:
+            self.data.append(item)
+            print(item)
+        return self.data
+
+    def update(self, id, employee):
+        employee_document =  {
+                               "id": employee.id,
+                               "name": employee.name,
+                               "age": employee.age
+                              }
+        container.replace_item(item=employee.id, body=employee_document)
+        print("Document updated successfully!")
+
+    def delete(self, id):
+        container.delete_item(id,id)
+        print("Document deleted successfully!")
+# Employee class
+
+# Example usage
